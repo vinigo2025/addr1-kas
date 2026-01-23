1edf18beab6be8122a261736f79ef6dbfd671d70
vinigo2025 <vinigo2025@gmail.com>
2026-01-23 12:43:05 +0300

    num threads

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ use std::time::Duration;
 use std::sync::atomic::Ordering;
 
 fn main() {
+    let plnum = thread::available_parallelism().expect("Sys err");
     println!("Working...");
 
-    thread::spawn(|| entr());
-    thread::spawn(|| entr());
-    thread::spawn(|| entr());
-    thread::spawn(|| entr());
+    for _i in 0..plnum.get() {
+        thread::spawn(|| entr());
+    }
 
     loop {
         thread::sleep(Duration::from_millis(500));
