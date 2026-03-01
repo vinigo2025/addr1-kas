c116e17beae117b4bdcddb76c014d729b8060e96
vinigo2025 <vinigo2025@gmail.com>
2026-03-01 21:10:44 +0300

    refact

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
 use kaspa_keygen::*;
+use std::sync::atomic::Ordering;
 use std::thread;
 use std::time::Duration;
-use std::sync::atomic::Ordering;
 
 fn main() {
     let plnum = thread::available_parallelism().expect("Sys err");
     println!("Working...");
 
-    for _i in 0..plnum.get() {
-        thread::spawn(|| entr());
+    for _ in 0..plnum.get() {
+        thread::spawn(entr);
     }
 
     loop {
         thread::sleep(Duration::from_millis(500));
         let cnt = CNT.load(Ordering::Relaxed);
-        if cnt > 0 { break; }
+        if cnt > 0 {
+            break;
+        }
     }
 }
